4022dbc1e7db7b979405908761a65a41b48ac258
Vovcnik <zaharov.v.a@gmail.com git config --global user.email zaharov.v.a@gmail.com>
2020-05-23 22:33:03 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,32 @@ $(function() {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                }
+            },
+        ]
     })
 
 
@@ -36,25 +62,25 @@ $(function() {
     });
 
     $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
-var id = $(this).attr('data-id');
-	$('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-	$('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-	$(this).addClass('active');
-	$('#'+id).addClass('active-tab').fadeIn();
-	return false;
-});
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
-$('input[type="file"], select').styler();
+    $('input[type="file"], select').styler();
 
-    $('.menu__btn').on('click', function(){
-      $('.menu__list').slideToggle();   
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
 
     });
-    
-    $('.header__btn-menu').on('click', function(){
-        $('.header__box').toggleClass('active');   
-  
-      });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.header__box').toggleClass('active');
+
+    });
 
 
 
